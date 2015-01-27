8bc6992ed7bd740f2e210c7714deef4248978b65
Schuyler Schwafel <saschwafel@gmail.com>
2015-01-27 15:19:37 -0800

    adding logout

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -84,7 +84,9 @@ def create(request):
 #    return {'one': one, 'project': 'learning_journal'}
 
 @view_config(route_name='auth', match_param='action=in', renderer='string', request_method='POST')
-def sign_in(request):
+@view_config(route_name='auth', match_param='action=out' renderer='string')
+
+def sign_in_out(request):
     login_form=None
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
