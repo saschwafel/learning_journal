1081ff486b84dc1dae58d1334b42fbc98f721d50
Schuyler Schwafel <saschwafel@gmail.com>
2015-01-27 14:25:14 -0800

    uncommenting admin

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -44,5 +44,5 @@ def main(argv=sys.argv):
         password = os.environ.get('ADMIN_PASSWORD', u'admin')
         password = manager.encode(password)
         #password = manager.encode(u'admin')
-        #admin = User(name=u'admin', password=password)
+        admin = User(name=u'admin', password=password)
         DBSession.add(admin)
