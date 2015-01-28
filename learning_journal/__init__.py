20248b54259fcdab42c1b288151a31c5f7e473e0
Schuyler Schwafel <saschwafel@gmail.com>
2015-01-27 18:03:40 -0800

    adding resources __init__

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,14 +1,14 @@
 import os
-from pyramid.config import Configurator
-from sqlalchemy import engine_from_config
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from .security import EntryFactory
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
 
 from .models import (
     DBSession,
     Base,
     )
+from .security import EntryFactory
 
 
 def main(global_config, **settings):
@@ -16,28 +16,22 @@ def main(global_config, **settings):
     """
     if 'DATABASE_URL' in os.environ:
         settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
-    #authentication_policy=AuthTktAuthenticationPolicy(secret)
-
+    engine = engine_from_config(settings, 'sqlalchemy.')
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     secret = os.environ.get('AUTH_SECRET', 'somesecret')
     config = Configurator(
         settings=settings,
-        authentication_policy=AuthTktAuthenticationPolicy('somesecret'),
+        authentication_policy=AuthTktAuthenticationPolicy(secret),
         authorization_policy=ACLAuthorizationPolicy(),
-        default_permission='view' 
+        default_permission='view'
     )
-    #config.include('pyramid_chameleon')
-    #
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
-
     config.add_route('home', '/', factory=EntryFactory)
     config.add_route('detail', '/journal/{id:\d+}', factory=EntryFactory)
     config.add_route('action', '/journal/{action}', factory=EntryFactory)
     config.add_route('auth', '/sign/{action}', factory=EntryFactory)
-    #config.add_route('edit', '/journal/{id:\d+}/edit')
-
     config.scan()
     return config.make_wsgi_app()
